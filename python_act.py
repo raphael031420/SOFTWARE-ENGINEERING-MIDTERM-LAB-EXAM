465764d842194e87e08ee33adfff12a34f663cdd
jan raphael gongora <jan.raphael.galicia.gongora@gmail.com>
2021-05-19 09:40:03 +0800

    Version 1 by GONGORA

---
+++ python_act.py
@@ -0,0 +1,32 @@
+print("""
+Name: Gongora, Jan Raphael G.
+Course and Section: SBIT 2-E
+Contact number: 09151640006
+Email Address: jan.raphael.galicia.gongora@gmail.com""")
+print("""
+   |   | 
+ X | X | O
+---+---+---
+ O | O |
+---+---+---  
+ X |   |   
+   |   |   
+""")
+print("""
+M     M  TTTTT  FFFFFF
+M M M M    T    F
+M  M  M    T    FFFF
+M     M    T    F
+M     M    T    F
+""")
+print("""
+ XXXXXXXXXXXXXXXX
+ X              X
+ X    _    _    X
+ X    O    O    X
+ X       |      X
+ X      _|      X
+ X              X
+ X   \______/   X
+ XXXXXXXXXXXXXXXX
+""")
